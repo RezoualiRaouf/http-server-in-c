8485397ee894b630b02d133fd349b97435e59017
RezoualiRaouf <raoufrezouali7@gmail.com>
2025-10-29 18:24:02 +0100

    fix : adding 10 to pointer and return it then casting it back

--- src/pthered.c
+++ src/pthered.c
@@ -6,8 +6,8 @@
 void *printing(void *value)
 {
   int *casted_value = (int*)value; 
-  casted_value =  value + 10;
-  return (void*)value;
+  *casted_value += 10;
+  return (void*)casted_value;
 }
 
 
@@ -15,9 +15,10 @@ int main()
 {
   pthread_t t;
   int value = 8;
-  void * retrval ;
+  void * retrval;
   pthread_create(&t,NULL,printing,(void *)&value);
   pthread_join(t, &retrval);
-  value = (int)retrval;
+  int *return_value = (int *)retrval;
+  printf("the value returned by the thread is %d \n",*return_value);
   return 0;
 }
