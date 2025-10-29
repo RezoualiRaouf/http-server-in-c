d428952ba45bcb0298bcba712b6710e016021d92
RezoualiRaouf <raoufrezouali7@gmail.com>
2025-10-29 18:44:28 +0100

    add : calculate the value with the first thread then pasit to the second thraed to calc again

--- src/pthered.c
+++ src/pthered.c
@@ -1,3 +1,4 @@
+
 #include <stdlib.h>
 #include <stdio.h>
 #include <pthread.h>
@@ -13,12 +14,23 @@ void *printing(void *value)
 
 int main()
 {
-  pthread_t t;
-  int value = 8;
-  void * retrval;
-  pthread_create(&t,NULL,printing,(void *)&value);
-  pthread_join(t, &retrval);
-  int *return_value = (int *)retrval;
-  printf("the value returned by the thread is %d \n",*return_value);
+  pthread_t t1,t2;
+  int value1 = 1;
+  int value2 = 2;
+
+  void *retrval1;
+  void *retrval2; 
+  
+  pthread_create(&t1,NULL,printing,(void *)&value1);
+  pthread_join(t1, &retrval1);
+  int *return_value = (int *)retrval1; 
+  printf("the value returned by the thread is %d\n",*return_value); 
+
+  pthread_create(&t2,NULL,printing,(void*)return_value);
+  pthread_join(t2,&retrval1);
+
+  return_value = (int *)retrval1;
+ 
+  printf("the value returned by the thread  is %d\n",*return_value);
   return 0;
 }
